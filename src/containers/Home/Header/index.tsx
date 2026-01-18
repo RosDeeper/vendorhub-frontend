77432c08b43fc3fc416c6f6fd9a41ab7e0cac735
Hoang Pham <hoanggg237@gmail.com>
2026-01-18 13:33:39 +0700

    update: logo vendor

--- src/containers/Home/Header/index.tsx
+++ src/containers/Home/Header/index.tsx
@@ -1,17 +1,26 @@
-'use client';
-
 import { Stack, Typography } from "@mui/material";
 import { LuMessageSquareText } from "react-icons/lu";
 import { FaRegBell } from "react-icons/fa";
+import Image from "next/image";
+import Link from "next/link";
 import { FaCaretDown } from "react-icons/fa6";
 
 import { DefaultAvatar } from "@/components/images";
 import { TEXT_SIZE } from "@/src/constants/text";
 import { Divider } from "@/components/common";
+import { SYSTEM_PATHS } from "@/src/constants/path";
 
 const Header = () => {
   return (
     <Stack className="header-container">
+      <Link href={SYSTEM_PATHS.dashboard}>
+        <Image 
+          src='/assets/vendor-black.png'
+          alt="VendorHub Logo"
+          width={160}
+          height={16}
+        />
+      </Link>
       <Stack direction='row' alignItems='center' gap={2} className="h-8">
         <LuMessageSquareText size={20} color="#584700" />
         <FaRegBell size={20} color="#584700" />
